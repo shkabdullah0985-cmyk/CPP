2f5b2fabf4b25e68fda7afaf7654aa63f3600193
shkabdullah0985-cmyk <shkabdullah0985@gmail.com>
2025-11-22 23:10:39 +0530

    Refactor order cancellation and feedback process

--- HOTEL_MANAGEMENT_SYSTEM.cpp
+++ HOTEL_MANAGEMENT_SYSTEM.cpp
@@ -7,7 +7,9 @@ using namespace std;
 int main(){
     string menu[]={"TEA","COFFEE","MILK","COLD DRINK"};
     int price[]={0,10,20,30,40};
+
     cout<<"MENU"<<endl;
+    
     
     for(int i = 1;i<5;i++){
     cout<<i<<"."<<menu[i-1]<<endl;
@@ -21,13 +23,16 @@ int main(){
     cout<<"ENTER A QUANTITY OF YOUR ORDER:- "<<endl;
     cin>>quantity;
     
-        int attempts = 0;
+    string feedback;
+    string reason; 
+    int attempts = 0;
     bool valid = false;
 
     while(attempts < 5){
-        cout<<"DO YOU WANT TO CANCEL THE ORDER (YES/NO) :- "<<endl;
+        cout<<"DO YOU WANT TO CANCEL OR REPLACE  THE ORDER (YES/NO) :- "<<endl;
+       
         cin>>cancelorder;
-
+        
         if(cancelorder=="YES" || cancelorder=="Y" || cancelorder=="yes" || cancelorder=="y"){
             valid = true;
             break;
@@ -35,6 +40,7 @@ int main(){
         else if(cancelorder=="NO" || cancelorder=="N" || cancelorder=="no" || cancelorder=="n"){
             valid = true;
             break;
+        
         }
         else{
             cout<<"INVALID INPUT! PLEASE TYPE YES OR NO"<<endl;
@@ -47,81 +53,119 @@ int main(){
         
     switch(order){
         case 1:
-        if( cancelorder=="YES" || cancelorder=="Y"){
+
+    if( cancelorder=="YES" || cancelorder=="Y"||cancelorder=="yes"||cancelorder=="y"){
+             
+                cout<<"IF YOU ARE CANCELING YOUR ORDER IT MAY CAN CHARGE YOU THE PENALTY AMOUNT"<<endl;
+                cout<<"ENTER YOUR REASON :- "<<endl;
+                cin>>reason;
+                if(reason=="replace"||reason=="REPLACE") {
+                cout<<"YOUR ORDER IS CANCEL"<<endl;    
+                }
+                else{
                 cout<<"YOUR ORDER IS CANCEL"<<endl;
                 cout<<"PENALTY AMOUNT IS 100 RS"<<endl;
-        }
-        else if (cancelorder=="NO"|| cancelorder=="N"||cancelorder=="n"||cancelorder=="no"){
-            cout<<"ORDER PLACE SUCESSFULLY :-) "<<endl;
-            cout<<"PRICE :- "<<price[1]<<endl;
-            cout<<"YOUR BILLING AMOUNT :- "<<quantity*price[1] <<endl;
-            cout<<"THANKYOU FOR VISITING ! "<<endl;  
+                }
+                
         }
         else {
-            cout<<"VISIT AGAIN :-("<<endl;
-        }
-        
-        break;
-        
-        case 2:
-        if( cancelorder=="YES" || cancelorder=="Y"){
-                cout<<"YOUR ORDER IS CANCEL"<<endl;
-                cout<<"PENALTY AMOUNT IS 100 RS"<<endl;
-        }
-        else if (cancelorder=="NO"|| cancelorder=="N"||cancelorder=="n"||cancelorder=="no"){
-            cout<<"ORDER PLACE SUCESSFULLY :-) "<<endl;
-            cout<<"PRICE :- "<<price[2]<<endl;
-            cout<<"YOUR BILLING AMOUNT :- "<<quantity*price[2] <<endl;
-            cout<<"THANKYOU FOR VISITING ! "<<endl;  
-        }
-        else {
-            cout<<"VISIT AGAIN :-("<<endl;
-        }
-        break;
-        
-        case 3:
-        if( cancelorder=="YES" || cancelorder=="Y"){
-                cout<<"YOUR ORDER IS CANCEL"<<endl;
-                cout<<"PENALTY AMOUNT IS 100 RS"<<endl;
-        }
-        else if (cancelorder=="NO"|| cancelorder=="N"||cancelorder=="n"||cancelorder=="no"){
-            cout<<"ORDER PLACE SUCESSFULLY :-) "<<endl;
-            cout<<"PRICE :- "<<price[3]<<endl;
-            cout<<"YOUR BILLING AMOUNT :- "<<quantity*price[3] <<endl;
-            cout<<"THANKYOU FOR VISITING ! "<<endl;  
-        }
-        else {
-            cout<<"VISIT AGAIN :-("<<endl;
-        }
-        
-        break;
-        
-        case 4:
-        if( cancelorder=="YES" || cancelorder=="Y"){
-                cout<<"YOUR ORDER IS CANCEL"<<endl;
-                cout<<"PENALTY AMOUNT IS 100 RS"<<endl;
-        }
-        else if (cancelorder=="NO"|| cancelorder=="N"||cancelorder=="n"||cancelorder=="no"){
             cout<<"ORDER PLACE SUCESSFULLY :-) "<<endl;
             cout<<"PRICE :- "<<price[4]<<endl;
             cout<<"YOUR BILLING AMOUNT :- "<<quantity*price[4] <<endl;
             cout<<"THANKYOU FOR VISITING ! "<<endl;  
+            cout<<" PLEASE ENTER YOUR FEEDBACK :- "<<endl;
+            cin>>feedback;
+            cout<<" THAKNYOU FOR YOUR RESPONSE :-)"<<endl;
+            cout<<"VISIT AGAIN :-)"<<endl;
+        }
+        break;
+        
+        case 2:
+        
+        if( cancelorder=="YES" || cancelorder=="Y"||cancelorder=="yes"||cancelorder=="y"){
+             
+                cout<<"IF YOU ARE CANCELING YOUR ORDER IT MAY CAN CHARGE YOU THE PENALTY AMOUNT"<<endl;
+                cout<<"ENTER YOUR REASON :- "<<endl;
+                cin>>reason;
+                if(reason=="replace"||reason=="REPLACE"){
+                cout<<"YOUR ORDER IS CANCEL"<<endl;    
+                }
+                else{
+                cout<<"YOUR ORDER IS CANCEL"<<endl;
+                cout<<"PENALTY AMOUNT IS 100 RS"<<endl;
+                }
+                
         }
         else {
-            cout<<"VISIT AGAIN :-("<<endl;
+            cout<<"ORDER PLACE SUCESSFULLY :-) "<<endl;
+            cout<<"PRICE :- "<<price[4]<<endl;
+            cout<<"YOUR BILLING AMOUNT :- "<<quantity*price[4] <<endl;
+            cout<<"THANKYOU FOR VISITING ! "<<endl;  
+            cout<<" PLEASE ENTER YOUR FEEDBACK :- "<<endl;
+            cin>>feedback;
+            cout<<" THAKNYOU FOR YOUR RESPONSE :-)"<<endl;
+            cout<<"VISIT AGAIN :-)"<<endl;
+        }
+        break;
+        
+        case 3:
+        
+        if( cancelorder=="YES" || cancelorder=="Y"||cancelorder=="yes"||cancelorder=="y"){
+                cout<<"IF YOU ARE CANCELING YOUR ORDER IT MAY CAN CHARGE YOU THE PENALTY AMOUNT"<<endl;
+                cout<<"ENTER YOUR REASON :- "<<endl;
+                cin>>reason;
+                if(reason=="replace"||reason=="REPLACE"){
+                cout<<"YOUR ORDER IS CANCEL"<<endl;    
+                }
+                else{
+                cout<<"YOUR ORDER IS CANCEL"<<endl;
+                cout<<"PENALTY AMOUNT IS 100 RS"<<endl;
+                }
+                
+        }
+        else {
+            cout<<"ORDER PLACE SUCESSFULLY :-) "<<endl;
+            cout<<"PRICE :- "<<price[4]<<endl;
+            cout<<"YOUR BILLING AMOUNT :- "<<quantity*price[4] <<endl;
+            cout<<"THANKYOU FOR VISITING ! "<<endl;  
+            cout<<" PLEASE ENTER YOUR FEEDBACK :- "<<endl;
+            cin>>feedback;
+            cout<<" THAKNYOU FOR YOUR RESPONSE :-)"<<endl;
+            cout<<"VISIT AGAIN :-)"<<endl;
+        }
+        break;
+        
+        case 4:
+        if( cancelorder=="YES" || cancelorder=="Y"||cancelorder=="yes"||cancelorder=="y"){
+                cout<<"IF YOU ARE CANCELING YOUR ORDER IT MAY CAN CHARGE YOU THE PENALTY AMOUNT"<<endl;
+                cout<<"ENTER YOUR REASON :- "<<endl;
+                cin>>reason;
+                if(reason=="replace"||reason=="REPLACE"){
+                cout<<"YOUR ORDER IS CANCEL"<<endl; 
+                
+                }
+                else{
+                cout<<"YOUR ORDER IS CANCEL"<<endl;
+                cout<<"PENALTY AMOUNT IS 100 RS"<<endl;
+                }
+                
+        }
+        else {
+            cout<<"ORDER PLACE SUCESSFULLY :-) "<<endl;
+            cout<<"PRICE :- "<<price[4]<<endl;
+            cout<<"YOUR BILLING AMOUNT :- "<<quantity*price[4] <<endl;
+            cout<<"THANKYOU FOR VISITING ! "<<endl;  
+            cout<<" PLEASE ENTER YOUR FEEDBACK :- "<<endl;
+            cin>>feedback;
+            cout<<" THAKNYOU FOR YOUR RESPONSE :-)"<<endl;
+            cout<<"VISIT AGAIN :-)"<<endl;
+            
         }
         break;
         
         default:
         cout<<"INVALID ORDER ! "<<endl;
     }
-    string feedback;
-        cout<<" PLEASE ENTER YOUR FEEDBACK :-) "<<endl;
-        cin>>feedback;
-        cout<<"FEEDBACK NOTED :- "<<feedback<<endl;
-        cout<<"WE WILL WORK ON IT :-) "<<endl;
-        cout<<" THAKNYOU FOR YOUR RESPONSE :-)"<<endl;
-    
     
     return 0;
 }
